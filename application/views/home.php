ae0ed67e35a2deb084d4ccc4f206c02f8fd48533
dindanavi <dinda.naviri@gmail.com>
2018-03-26 22:18:22 +0700

    dindanavi, http://www.pemrograman-framework/id

---
+++ application/views/home.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+  <title>CodeIgniter</title>
+  <link rel="stylesheet" media="all"  type="text/css" href="<?php echo base_url() ?>assets/css/bootstrap.min.css">
+<link rel="stylesheet" media="all"  type="text/css" href="<?php echo base_url() ?>assets/css/bootstrap.css">
+<link rel="stylesheet" media="all"  type="text/css" href="<?php echo base_url() ?>assets/css/bootstrap-theme.css">
+  <script type="text/javascript" src="<?php echo base_url() ?>assets/js/jquery.js"></script>
+  <script type="text/javascript" src="<?php echo base_url() ?>assets/js/bootstrap.js"></script>
+</head>
+<body>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="#">NAVI</a>
+      </div>
+      <ul class="nav navbar-nav">
+          <li class="active"><a href="<?php echo site_url() ?>welcome">Home</a></li>
+        <li><a href="<?php echo site_url() ?>about">About</a></li>
+        
+      </ul>
+     
+    </div>
+  </nav>
+  <table border="0" align="center">
+    <tr>
+      <td colspan="2"><h2><p align="center"> <b> AMEBA PIGG INDONESIA </b> </p></h2></td>      
+    </tr>
+    <tr>
+      <td colspan="2"></td>      
+    </tr>
+      <tr>
+        <td> <img src="1.jpg"> </td>
+        <td> <br><br>
+        Game asal Jepang ini sebenarnya ditargetkan untuk pemain Jepang. <br>
+        Tidak seperti pico yang berbahasa utama english, dalam pigg kamu harus memiliki <br>
+        usaha ekstra untuk memahami maksud dari game tersebut. Pigg hanya akan memberikan <br>
+        penjelasan berbahasa jepang dalam ameblog (blog pigg yang disediakan sebagai petunjuk bagi pemain). <br>
+        Namun tak apa, berbekal browser chrome kamu dapat mentranslate isi penjelasan dalam ameblog. <br>
+        Walaupun tak dapat mentraslate seluruh game tak menjadi masalah, kami sudah terbiasa.
+        </td>
+      </tr>
+    <tr>
+      <td colspan="2"><br><br><p align="center"><img src="1.jpg"></p></td>      
+    </tr>
+  </table>
+
+</div>
+</body>
+</html>
